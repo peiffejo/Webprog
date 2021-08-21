6b011d0eab662d28f8c8c5216d7d28b6a5a18037
peiffejo <johannes-hubert.peiffer@informatik.hs-fulda.de>
2021-08-21 21:19:41 +0200

    MHD

--- routes/nav.js
+++ routes/nav.js
@@ -3,15 +3,26 @@ var router = express.Router();
 
 
 // Array for the Products
+const today = new Date();
+const MHD = new Date(2021, 7, 20);
+
+//get Pic
+var getData = function () {
+  return 'https://www.publicdomainpictures.net/pictures/210000/t2/beach-chairs-and-sea-1488302392D7r.jpg';
+}
 
 let products = [{
+  ID: 1,
   name: "Coca Cola",
-  img: 3,
+  img: 'https://www.w3schools.com/images/w3schools_green.jpg',
   amount: 5,
-  MHD: 30/02/2022,
+  today: today.getTime(),
+  MHD: Math.round((MHD.getTime() -today.getTime())/(1000*60*60*24)),
 }];
+
 router.get('/', function(req, res, next) {
-  res.render('listProducts', { products: products });
+  
+  res.render('listProducts', { products: products} );
   });
 
 router.get('/list', function (req, res) {
@@ -20,6 +31,7 @@ router.get('/list', function (req, res) {
 
 router.get('/create', function (req, res) {
   res.render('createProduct', { products: products });
+
 });
   
 module.exports = router;
