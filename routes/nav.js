6f59de36d9c5e6121a0575d85a852e4e9b6ad303
peiffejo <72967199+peiffejo@users.noreply.github.com>
2021-08-30 21:43:37 +0200

    load pic from website

--- routes/nav.js
+++ routes/nav.js
@@ -5,29 +5,26 @@ var router = express.Router();
 // Array for the Products
 const today = new Date();
 
-//get Pic
-var getData = function () {
-  return 'https://www.publicdomainpictures.net/pictures/210000/t2/beach-chairs-and-sea-1488302392D7r.jpg';
-}
+
 
 let products = [{
   ID: 1,
   name: "Coca Cola",
-  img: 'https://www.w3schools.com/images/w3schools_green.jpg',
+  img: 'https://as2.ftcdn.net/v2/jpg/01/83/31/03/500_F_183310393_A7hcmA9HKSLnym7VUPwhgmByrcvxJRoK.jpg',
   amount: 5,
   MHD: Math.round((new Date(2021, 10, 20).getTime() -today.getTime())/(1000*60*60*24)),
 },
 {
   ID: 2,
   name: "Mango",
-  img: 'https://www.w3schools.com/images/w3schools_green.jpg',
+  img: 'https://www.publicdomainpictures.net/pictures/170000/velka/mango-14606845838Oe.jpg',
   amount: 10,
   MHD: Math.round((new Date(2021, 4, 20).getTime() -today.getTime())/(1000*60*60*24)),
 },
 {
   ID: 3,
-  name: "Coca Cola",
-  img: 'https://www.w3schools.com/images/w3schools_green.jpg',
+  name: "Kiwi",
+  img: 'https://www.publicdomainpictures.net/pictures/70000/velka/kiwi-fruit-1389439824dsq.jpg',
   amount: 5,
   MHD: Math.round((new Date(2022, 6, 23).getTime() -today.getTime())/(1000*60*60*24)),
 }];
