9cc79dbada73c974a8f32d9393d495c5790cc8e1
peiffejo <johannes-hubert.peiffer@informatik.hs-fulda.de>
2021-09-11 22:34:45 +0200

    Grundstruktur

---
+++ public/javascript/JS.js
@@ -0,0 +1,77 @@
+//plus 1 bei Menge
+document.getElementById('plusOne')?.addEventListener('click', function () {
+    let ID = document.getElementById('deleteBtn').value;
+    let getAmount = parseInt((document.getElementById('plusOne').value))+1;
+    let data = { 
+        amount: getAmount
+    };
+      fetch('/nav/plusOne/' + ID, {
+        method: 'Put',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      }).then(function (response) {
+          response.text().then(function (text) {
+            window.location = '/singleProduct/'+ID;
+          });
+        
+      });
+  });
+
+  //minus 1 bei Menge und löscht falls 0
+document.getElementById('minusOne')?.addEventListener('click', function () {
+    let ID = document.getElementById('deleteBtn').value;
+    let getAmount = parseInt((document.getElementById('minusOne').value))-1;
+    let data = { 
+        amount: getAmount
+    };
+      fetch('/nav/plusOne/' + ID, {
+        method: 'Put',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      }).then(function (response) {
+          if(getAmount > 0){
+          response.text().then(function (text) {
+            window.location = '/singleProduct/'+ID;
+          });
+        }else{
+          response.text().then(function (text) {
+            fetch('/nav/delete/' + ID, {
+                method: 'DELETE',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                        },
+                    }).then(function (response) {
+                    response.text().then(function (text) {
+                    window.location = '/list';
+                        });
+                    });
+          }); 
+        }
+        
+      });
+  });
+
+//löscht das Produkt
+document.getElementById('deleteBtn')?.addEventListener('click', function () {
+    let ID = document.getElementById('deleteBtn').value;
+            alert(ID);
+            fetch('/nav/delete/' + ID, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                    },
+                }).then(function (response) {
+                response.text().then(function (text) {
+                window.location = '/list';
+                    });
+                });
+            });
+
--- routes/nav.js
+++ routes/nav.js
@@ -8,54 +8,84 @@ const today = new Date();
 
 function MHD(DateFromMHD){
   if(DateFromMHD >= 0){
-      return [{
-        infoCss: true,
-        text: "Your food lifetime has " + DateFromMHD + " days left",
-      }]
-       
-      }else{
-        return [{
-          infoCss: false,
-          text: "Your food lifetime has " + Math.abs(DateFromMHD) + " days left",
-        }]
+      return "Your food lifetime has " + DateFromMHD + " days left";     
+  }
+      return "Your food lifetime has " + Math.abs(DateFromMHD) + " days left";
     //  return "The food MHD date is over since " + Math.abs(DateFromMHD) + " days" ;
       }
-}
+let CocaCola = new Date(2021, 7, 20);
+let Mango = new Date(2021, 4, 20);
+let Kiwi = new Date(2022, 6, 23);
 
 let products = [{
   ID: 1,
   name: "Coca Cola",
   img: 'https://as2.ftcdn.net/v2/jpg/01/83/31/03/500_F_183310393_A7hcmA9HKSLnym7VUPwhgmByrcvxJRoK.jpg',
   amount: 5,
-  MHD: MHD(Math.round((new Date(2021, 7, 20).getTime() -today.getTime())/(1000*60*60*24))),
+  MHD: MHD(Math.round((CocaCola.getTime() -today.getTime())/(1000*60*60*24))),
+  MHDdate: [CocaCola.getDate(), CocaCola.getMonth(), CocaCola.getFullYear()]
 },
 {
   ID: 2,
   name: "Mango",
   img: 'https://www.publicdomainpictures.net/pictures/170000/velka/mango-14606845838Oe.jpg',
   amount: 10,
-  MHD: MHD(Math.round((new Date(2021, 4, 20).getTime() -today.getTime())/(1000*60*60*24))),
+  MHD: MHD(Math.round((Mango.getTime() -today.getTime())/(1000*60*60*24))),
+  MHDdate: [Mango.getDate(), Mango.getMonth(), Mango.getFullYear()],
 },
 {
   ID: 3,
   name: "Kiwi",
   img: 'https://www.publicdomainpictures.net/pictures/70000/velka/kiwi-fruit-1389439824dsq.jpg',
   amount: 5,
-  MHD: MHD(Math.round((new Date(2022, 6, 23).getTime() -today.getTime())/(1000*60*60*24))),
+  MHD: MHD(Math.round((Kiwi.getTime() -today.getTime())/(1000*60*60*24))),
+  MHDdate: [Kiwi.getDate(), Kiwi.getMonth(), Kiwi.getFullYear()]
 }];
 
 router.get('/', function(req, res, next) {
-  
   res.render('listProducts', { products: products} );
   });
 
+//zeigt einzelenes Produkt
+  router.get('/singleProduct/:id', function (req, res) {
+    const ID = products.find(c => c.ID === parseInt(req.params.id));
+    res.render('singleProduct', { ID });
+});
+
+//Zeigt alle Produkte
 router.get('/list', function (req, res) {
   res.render('listProducts', { products: products });
 });
 
+//zeigt die Seite zum editieren 
+router.get('/edit/:id', function (req, res) {
+  const ID = products.find(c => c.ID === parseInt(req.params.id));
+  res.render('editProduct', { ID });
+});
+
 router.get('/create', function (req, res) {
   res.render('createProduct', { products: products });
+});
 
+//löscht bestimmtes Produkt
+router.delete('/delete/:postID', function (req, res, next) {
+  let post = products.find(p => p.ID === parseInt(req.params.postID));
+  if (!post) {
+      res.status(404).send('Post not found');
+  }
+  let index = products.indexOf(post);
+  products.splice(index, 1);
+  res.send(products);
+});
+
+//
+router.put('/plusOne/:postID', function (req, res, next) {
+  let post = products.find(p => p.ID === parseInt(req.params.postID));
+  if (!post) {
+      res.status(404).send('Post not found');
+  }
+  post.amount = req.body.amount;
+  res.send(products);
 });
   
 module.exports = router;
